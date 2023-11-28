4820382d078104d28eda55d25ca8713a4a1f42d3
Anabelagarcia <anabelanams@gmail.com>
2023-11-28 10:05:02 -0400

    Create propri
    Separação dos componentes e home por pasta. Criação das propriedades nome, genero, descricao, sinopse, id, link e lista. Criação de conteúdo da header.

---
+++ src/Cadastro/Cadastro.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Cadastro(){
+     
+    const [nome, setNome] = useState("")
+    const [genero, setgenero] = useState("")
+    const [descricao, setDescricao] = useState("")
+    const [sinopse, setSinopse] = useState("")
+    const [id, setId] = useState(1)
+    const [link, setLink] = useState("")
+    const [lista, setLista] = useState([])
+}
+
+
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+  return(
+
+    <div class= "header">
+         <h1>Ola</h1>
+    </div>
+  );
+}
+
+
